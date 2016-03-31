820144cfe1a1d35a1f5245b140901e013c5134f1
will <wilfredrthibodeau@gmail.com>
2016-03-30 22:05:32 -0400

    first commit

--- www/js/map.js
+++ www/js/map.js
@@ -1,21 +1,78 @@
+
 $(document).ready(function() {
 $('#map').goMap({
-    latitude : 45.53940,
-    longitude : -122.59025,
+    latitude : 35.595027,
+	longitude :  -82.551664,
     zoom : 16,
-    scaleControl : true,
-    maptype : 'SATELLITE'
+    
+    maptype : 'ROADMAP'
 }); // end goMap
 
 
-$.goMap.createMarker({  
-	   latitude : 45.,
-	   longitude :  -122.59473,
+var markers = [ 
+	   
+		{
+			latitude: 35.594551,
+			longitude: -82.553574,
+			title: 'DrHumor Building',
+				html: {
+					content: '<h3>DrHumor Building</h3><p>Location 3</p>',
+					popup: true
+				}
+		},
+		{
+			latitude: 35.594753,
+			longitude: -82.552648,
+			title: 'Top Hat',
+				html: {
+					content: '<img src="./img/haticon.jpg"><h3>Top Hat</h3><p>Location 2</p>',
+					popup: true
+				}
+		},
+		{
+			latitude: 35.595194,
+			longitude: -82.554869,
+			title: 'Flat Iron',
+				html: {
+					content: '<h3>Flat Iron</h3><p>Location 4</p>',
+					popup: true
+				}
+		},
+		{
+			latitude: 35.594495,
+			longitude: -82.556310,
+			title: 'Cats',
+				html: {
+					content: '<h3>Cats</h3><p>Location 5</p>',
+					popup: true
+				}
+		},
+		{
+	   latitude : 35.595027,
+	   longitude :  -82.551664,
 	   title : 'ho',
-	   html : {
-	     content : '<h2>Hol</h2><p>Par 4, 346 Yards</p>',
-		 popup : true   
-	   }
-}); // end createMarker
+		   html : {
+		     content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
+			 popup : true  
+		   }
+		}
+
+	]	
+$('#map').goMap({
+	latitude : 35.595027,
+	longitude :  -82.551664,
+    zoom : 16,
+}); // end goMap
+$.each(markers,function(i,marker){
+	$.goMap.createMarker(marker);
+}); //end each
 
 }); // end ready
+
+
+
+
+
+
+
+
