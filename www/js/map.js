3eab27591fe6b6ab842d5cef4ddbff9de6d81b39
unknown <wilfredrthibodeau@BAL00319.stu.asheville.cc.nc.us>
2016-03-31 19:51:12 -0400

    corrected ajax

--- www/js/map.js
+++ www/js/map.js
@@ -10,15 +10,14 @@ $('#map').goMap({
 
 
 var markers = [ 
-	   
 		{
-			latitude: 35.594551,
-			longitude: -82.553574,
-			title: 'DrHumor Building',
-				html: {
-					content: '<h3>DrHumor Building</h3><p>Location 3</p>',
-					popup: true
-				}
+		latitude: 35.594551,
+		longitude: -82.553574,
+		title: 'DrHumor Building',
+			html: {
+				content: '<h3>DrHumor Building</h3><p>Location 3</p>',
+				popup: true
+			}
 		},
 		{
 			latitude: 35.594753,
@@ -48,13 +47,67 @@ var markers = [
 				}
 		},
 		{
-	   latitude : 35.595027,
-	   longitude :  -82.551664,
-	   title : 'ho',
-		   html : {
-		     content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
-			 popup : true  
-		   }
+		   	latitude : 35.595027,
+		   	longitude :  -82.551664,
+		   	title : 'Monument',
+			   	html : {
+			     	content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
+				 	popup : true  
+			   }
+		},
+		{
+		   	latitude : 35.596097,
+		   	longitude :  -82.556365,
+		   	title : 'Bronze Book',
+			   	html : {
+			     	content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
+				 	popup : true  
+			   }
+		},
+		{
+		   	latitude : 35.597109,
+		   	longitude :  -82.555620,
+		   	title : 'Civic Center',
+			   	html : {
+			     	content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
+				 	popup : true  
+			   }
+		},
+		{
+		   	latitude : 35.596101,
+		   	longitude :  -82.554954,
+		   	title : 'Medicinal Herbs',
+			   	html : {
+			     	content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
+				 	popup : true  
+			   }
+		},
+		{
+		   	latitude : 35.596402,
+		   	longitude :  -82.553018,
+		   	title : 'Market Place',
+			   	html : {
+			     	content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
+				 	popup : true  
+			   }
+		},
+		{
+		   	latitude : 35.596917,
+		   	longitude :  -82.551124,
+		   	title : 'Art In Motion',
+			   	html : {
+			     	content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
+				 	popup : true  
+			   }
+		},
+		{
+		   	latitude : 35.595822,
+		   	longitude :  -82.550781,
+		   	title : 'Civic Price',
+			   	html : {
+			     	content : '<img src="./img/monumenticon.jpg"><h3>Pack Monument</h3><p>Location 1</p>',
+				 	popup : true  
+			   }
 		}
 
 	]	
